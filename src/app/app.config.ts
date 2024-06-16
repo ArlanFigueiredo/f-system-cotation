01d71a88f31ecd8bbfdf712c552309149eb7a490
ArlanFigueiredo <arlan.carloz@gmail.com>
2024-06-16 13:41:23 -0300

    clear app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { MenuItem, MessageService } from 'primeng/api';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideNativeDateAdapter(), provideAnimations(), provideHttpClient(), MessageService],
 };
